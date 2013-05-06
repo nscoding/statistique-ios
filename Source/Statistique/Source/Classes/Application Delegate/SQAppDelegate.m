6c5a056cb983188b2edc003c1be23010c2b69782
nscoding <patrick@nscoding.co.uk>
2013-05-06 12:11:04 +0200

    Started 3.05 added App rater and Settings

--- Source/Statistique/Source/Classes/Application Delegate/SQAppDelegate.m
+++ Source/Statistique/Source/Classes/Application Delegate/SQAppDelegate.m
@@ -6,9 +6,14 @@
 //  Copyright (c) 2013 Patrick Chamelo - nscoding. All rights reserved.
 //
 
+
 #import "SQAppDelegate.h"
-#import "TestFlight.h"
 #import "SQViewController.h"
+
+
+#import "TestFlight.h"
+#import "Appirater.h"
+
 
 @implementation SQAppDelegate
 
@@ -19,11 +24,37 @@
     [TestFlight takeOff:@"4271bc9a-cbf6-453f-8138-0de102d1f91c"];
 #endif
     
+    NSString *appVersionString = [[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleVersion"];
+    [[NSUserDefaults standardUserDefaults] setObject:appVersionString forKey:@"currentVersionKey"];
+    
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
     self.viewController = [[SQViewController alloc] initWithNibName:@"SQViewController" bundle:nil];
     self.window.rootViewController = self.viewController;
     [self.window makeKeyAndVisible];
+    
+    [Appirater setAppId:@"321094050"];
+    [Appirater setDaysUntilPrompt:6];
+    [Appirater setUsesUntilPrompt:3];
+    [Appirater setSignificantEventsUntilPrompt:0];
+    [Appirater setTimeBeforeReminding:3];
+    [Appirater setUsesAnimation:YES];
+    
+#if !APPSTORE
+    [Appirater setDebug:YES];
+#else
+    [Appirater setDebug:NO];
+#endif
+    
+    double delayInSeconds = 1.0;
+    dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW,
+                                            (int64_t)(delayInSeconds * NSEC_PER_SEC));
+    
+    dispatch_after(popTime, dispatch_get_main_queue(), ^(void)
+    {
+        [Appirater appLaunched:YES];
+    });
+
     return YES;
 }
 
