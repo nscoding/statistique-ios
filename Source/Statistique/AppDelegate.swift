cf3bbaf8eaf82c90e2904a6f8216d02170f9ef9f
nscoding <patrick@nscoding.co.uk>
2014-06-08 00:37:59 +0200

    OTHER: Moving files, preparing for Swift re-write

---
+++ Source/Statistique/AppDelegate.swift
@@ -0,0 +1,46 @@
+//
+//  AppDelegate.swift
+//  Statistique
+//
+//  Created by Patrick Chamelo on 08/06/14.
+//  Copyright (c) 2014 Patrick Chamelo. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate
+{
+    var window: UIWindow?
+
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: NSDictionary?) -> Bool
+    {
+        self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        // Override point for customization after application launch.
+        self.window!.backgroundColor = UIColor.whiteColor()
+        self.window!.makeKeyAndVisible()
+        return true
+    }
+
+    func applicationWillResignActive(application: UIApplication)
+    {
+    }
+
+    func applicationDidEnterBackground(application: UIApplication)
+    {
+    }
+
+    func applicationWillEnterForeground(application: UIApplication)
+    {
+    }
+
+    func applicationDidBecomeActive(application: UIApplication)
+    {
+    }
+
+    func applicationWillTerminate(application: UIApplication)
+    {
+    }
+}
+
